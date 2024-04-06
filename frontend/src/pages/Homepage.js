4118d16ea2b1236c824a09fc70b41d1ba0f20edd
Gamak Grover <you@example.com>
2024-04-06 15:16:45 +0530

    animation

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,56 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Grid, Box, Button } from '@mui/material';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Grid, Box, Button } from "@mui/material";
+import styled from "styled-components";
 import Students from "../assets/students.svg";
-import { LightPurpleButton } from '../components/buttonStyles';
+import { LightPurpleButton } from "../components/buttonStyles";
 
 const Homepage = () => {
-    return (
-        <StyledContainer>
-            <Grid container spacing={0}>
-                <Grid item xs={12} md={6}>
-                    <img src={Students} alt="students" style={{ width: '100%' }} />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <StyledPaper elevation={3}>
-                        <StyledTitle>
-                            Welcome to
-                            <br />
-                            School Management
-                            <br />
-                            System
-                        </StyledTitle>
-                        <StyledText>
-                            Streamline school management, class organization, and add students and faculty.
-                            Seamlessly track attendance, assess performance, and provide feedback.
-                            Access records, view marks, and communicate effortlessly.
-                        </StyledText>
-                        <StyledBox>
-                            <StyledLink to="/choose">
-                                <LightPurpleButton variant="contained" fullWidth>
-                                    Login
-                                </LightPurpleButton>
-                            </StyledLink>
-                            <StyledLink to="/chooseasguest">
-                                <Button variant="outlined" fullWidth
-                                    sx={{ mt: 2, mb: 3, color: "#7f56da", borderColor: "#7f56da" }}
-                                >
-                                    Login as Guest
-                                </Button>
-                            </StyledLink>
-                            <StyledText>
-                                Don't have an account?{' '}
-                                <Link to="/Adminregister" style={{color:"#550080"}}>
-                                    Sign up
-                                </Link>
-                            </StyledText>
-                        </StyledBox>
-                    </StyledPaper>
-                </Grid>
-            </Grid>
-        </StyledContainer>
-    );
+  return (
+    <StyledContainer>
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={6}>
+          <img
+            src={Students}
+            alt="students"
+            style={{ width: "100%" }}
+            className="animate__animated animate__slower animate__delay-3s animate__fadeIn"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <StyledPaper elevation={3}>
+            <StyledTitle className="animate__animated animate__slower animate__backInLeft">
+              Welcome to
+              <br />
+              School Management
+              <br />
+              System
+            </StyledTitle>
+            <StyledText className="animate__animated animate__slower animate__backInLeft">
+              Streamline school management, class organization, and add students
+              and faculty. Seamlessly track attendance, assess performance, and
+              provide feedback. Access records, view marks, and communicate
+              effortlessly.
+            </StyledText>
+            <StyledBox>
+              <StyledLink to="/choose">
+                <LightPurpleButton
+                  className="animate__animated animate__slower animate__delay-3s animate__fadeIn"
+                  variant="contained"
+                  fullWidth
+                >
+                  Login
+                </LightPurpleButton>
+              </StyledLink>
+              <StyledLink
+                className="animate__animated animate__slower animate__delay-3s animate__fadeIn"
+                to="/chooseasguest"
+              >
+                <Button
+                  id="home-btn"
+                  variant="outlined"
+                  fullWidth
+                  sx={{
+                    mt: 2,
+                    mb: 3,
+                    color: "#7f56da",
+                    borderColor: "#7f56da",
+                  }}
+                >
+                  Login as Guest
+                </Button>
+              </StyledLink>
+              <StyledText>
+                Don't have an account?{" "}
+                <Link to="/Adminregister" style={{ color: "#550080" }}>
+                  Sign up
+                </Link>
+              </StyledText>
+            </StyledBox>
+          </StyledPaper>
+        </Grid>
+      </Grid>
+    </StyledContainer>
+  );
 };
 
 export default Homepage;
@@ -71,7 +92,7 @@ const StyledBox = styled(Box)`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content:center;
+  justify-content: center;
   gap: 16px;
   padding: 24px;
 `;
@@ -89,7 +110,7 @@ const StyledTitle = styled.h1`
 const StyledText = styled.p`
   /* color: #550080; */
   margin-top: 30px;
-  margin-bottom: 30px; 
+  margin-bottom: 30px;
   letter-spacing: normal;
   line-height: normal;
 `;
